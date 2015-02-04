d918beb34d532e08ad7ba8e986ff5851d9e04988
Steven Das <steven@zirtual.com>
2015-02-04 17:47:26 -0600

    First pass at boosty stuff (still need to add boost).

--- vigenere.hpp
+++ vigenere.hpp
@@ -5,5 +5,8 @@ using namespace std;
 
 string encrypt(const string& plaintext, const string& keyword)
 {
-  return "XXXX: Write me!";
+  return "XXXX: Write me! Plaintext " +
+    boost::to_upper(plaintext) +
+    " Keyword " +
+    boost::to_upper(keyword);
 }
