462a05328bdb2314cfd3537c1af0b6d246bfc371
Steven Das <steven@zirtual.com>
2015-02-05 23:20:43 -0600

    Got string concatenation to work correctly. Get boost uppercase to work next.

--- vigenere.hpp
+++ vigenere.hpp
@@ -5,8 +5,5 @@ using namespace std;
 
 string encrypt(const string& plaintext, const string& keyword)
 {
-  return "XXXX: Write me! Plaintext " +
-    boost::to_upper(plaintext) +
-    " Keyword " +
-    boost::to_upper(keyword);
+  return "XXXX: Write me! Plaintext " + plaintext;
 }
