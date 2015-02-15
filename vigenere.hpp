004b7b14011c35188f26170c17d26f4362e85451
Steven Das <steven@zirtual.com>
2015-02-14 23:05:39 -0600

    Removed length restriction.

--- vigenere.hpp
+++ vigenere.hpp
@@ -7,9 +7,6 @@ string encrypt(const string& plaintext, const string& keyword)
 {
   /* Returns the plaintext encrypted using the given keyword and the Vigenere
    * Cipher (http://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher).
-   *
-   * For now, assumes that the length of the plaintext is greater than or equal
-   * to the length of the keyword.
    */
 
   string keyphrase = ""
