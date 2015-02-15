fe0c65050bcfeaa5cff8a6c230253d5bac9f2b2a
Steven Das <steven@zirtual.com>
2015-02-14 23:04:39 -0600

    Function comment and start of for loop.

--- vigenere.hpp
+++ vigenere.hpp
@@ -5,8 +5,19 @@ using namespace std;
 
 string encrypt(const string& plaintext, const string& keyword)
 {
-  string keyword = boost::algorithm::to_upper_copy<string>(keyword);
+  /* Returns the plaintext encrypted using the given keyword and the Vigenere
+   * Cipher (http://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher).
+   *
+   * For now, assumes that the length of the plaintext is greater than or equal
+   * to the length of the keyword.
+   */
 
+  string keyphrase = ""
+  int reps = plaintext.length() / keyword.length() + 1
+  for (int i = 0; i < plaintext.length() /
+  {
+
+  }
 
   string plaintext = boost::algorithm::to_upper_copy<string>(plaintext);
 
